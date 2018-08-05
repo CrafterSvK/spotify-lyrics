1fd3d91a4175d3b03afce66e2669e8d07735d7ad
Jakub Janek <janek.jakub@gmail.com>
2018-08-05 22:52:32 +0200

    Rewrite for a WebAPI

--- README.md
+++ README.md
@@ -4,33 +4,23 @@ Bash script to show lyrics from spotify name of song and artist name.
 ## Dependencies
 * w3m (for displaying lyrics)
 * wget (for downloading lyrics from AZLyrics.com)
-
-### If using spotify metadata
-* DBUS (to get spotify name of artist and song)
-* Spotify (obviously)
+* jq (for json WebAPI parsing)
+* sed and curl
 
 ## Installation
 Clone repository into Computer.
 Make symlink into /bin folder so you can run it from terminal everywhere.
 
-## Usage with spotify
-* Run spotify.
-* Play a song.
-* Run `getLyrics.sh -s`.
+## Usage
+* Get auth token from [WebAPI](https://developer.spotify.com/console/get-users-currently-playing-track/), click on GET TOKEN and check user-read-currently-playing
+* Write it into `getLyrics` key variable (2nd line)
 
-## Usage without spotify
-* Run `getLyrics.sh`.
-* Give it artist and song name
-
-## Override (Songs that violates with algorithm)
-* If you find a song that violates with algorithm for example AWOLNATION - Hollow Moon (Bad Wolf) just add it into override.txt file 
+## Override (Songs that violates with algorithm) currently unavailable
+* If you find a song that violates with algorithm for example AWOLNATION - Hollow Moon (Bad Wolf) just add it into override.txt file
 * How to write override (all in lowercase): ArtistName-SongNameAfterAlgorithm:SongOverride
 
 ## WARNING
 Don't use this program commercialy AZLyrics's license is prohibiting the usage of their lyrics.
 
 ## TODO list
-* Make script MacOS compatible (Which is hard because there is no DBus on Mac defaultly)
-
-## Credits
-* [@csssuf](https://github.com/csssuf) - For awk script.
+* Add Windows installation tutorial
--- getLyrics.sh
+++ getLyrics.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-
 dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
-function get_info {
-	#param: $1 - Sets which metadata should be given
-	if [[ -z $1 ]]; then
-		local result="NULL"
-		echo "$result"
-	elif [[ $1 == "song" ]]; then
-		local result=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/spotify_song.awk | tail -n1 | cut -d':' -f2)
-		echo "$result"
-	elif [[ $1 == "artist" ]]; then
-		local result=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/spotify_song.awk | head -n 1 | cut -d':' -f2)
-		echo "$result"
-	fi
+key='BQBalmmv_2oc8cvOxwt8hIUkxxCCXoUgk-GH80C941QfNA63PrJMbp4SgdsHfH8Z7g23t73Cp_UFlYwVO2PesP-Q4s1qgUH8Wr0Wv64vlYx9Woex7fbO5oL6gnw702qPDyq45ypnw_R_DXQCPl54EY4'
+
+function getInfo {
+    local endpoint=$(curl -s -X "GET" "https://api.spotify.com/v1/me/player/currently-playing?market=SK" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $key")
+
+    echo $endpoint
+}
+
+function adjust {
+	local pre_result="$(echo $1 | sed -r -e 's/[^A-Za-z()]|\((.*?)\)//gi')"
+    local result=${pre_result,,}
+
+    echo $result
+}
+
+function join {
+	#param: $1 joins as the first one
+	#param: $2 joins to $1
+	local result=$1"\ \-\ "$2
+	echo $result
 }
 
 function escape {
@@ -27,113 +34,26 @@ function escape {
 	fi
 }
 
-function trim {
-	#param: $1 - This string will be trimmed from unuseful characters
-	local trim1="${1//[[:space:]]/}"
-	local trim2="${trim1%\?}"
-	local trim3="${trim2//\'}"
-	local trim4="${trim3//\/}"
-	local trim5="${trim4%%-*}"
-	local trim6="${trim5//.}"	
-	local trim7="${trim6//\!}"
-	local trim8="${trim7//,}"
-	local trim9="${trim8//&}"
-	local trim12=$trim9
+echo Loading lyrics...
+info=$(getInfo)
 
-	#trim brackets if they are available
-	if [[ $trim9 == *[{}\(\)\[\]]* ]]; then
-		local string_brackets="$(echo $trim9 | cut -d "(" -f2 | cut -d ")" -f1)"
-		local trim10="${trim9//$string_brackets}"
-		local trim11="${trim10//\(}"
-		local trim12="${trim11//\)}"
-	fi
-
-	local result="${trim12,,}"
-	echo $result
-}
-
-function trim_the {
-	#param: $1 - This string will be trimmed from The
-	
-    detect=$(echo "${1}" | cut -c1-3)
-    if [[ $detect == "the" ]]; then
-        local trim="${1//the}"
-	    local result=$trim
-
-	    echo $result
-    else
-        echo ${1}
-    fi
-}
-
-function detect_override {
-    #Overriding songs that are violating the algorithm
-
-    #param: $1 - Passing artist name for override
-    #param: $2 - Passing song name for override  
-
-    local file="$(cat $dir/override.txt)"
-    local artist_name_search="$(echo $file | grep $1)"
-    local song_name_search="$(echo $artist_name_search | grep $2)"
-    
-    local song="$(echo $song_name_search | cut -d ":" -f2)" 
-
-    local result=$song 
-    echo $result
-}
-
-function join {
-	#param: $1 joins as the first one
-	#param: $2 joins to $1
-	local result=$1"\ \-\ "$2
-	echo $result
-}
-
-function get_spotify {
-    artist_raw=$(get_info artist)
-    artist_with_the=$(trim "$artist_raw")
-    artist=$(trim_the "$artist_with_the")
-
-    song_raw=$(get_info song)
-    song=$(trim "$song_raw")
-}
-
-function get_manual {
-    artist_raw=$1
-    artist_with_the=$(trim "$artist_raw")
-    artist=$(trim_the "$artist_with_the")   
-
-    song_raw=$2
-    song=$(trim "$song_raw")
-}
+track_name=$(echo $info | jq -r ".item.name")
+author_name=$(echo $info | jq -r ".item.album.artists[].name")
 
 rm -rf /tmp/lyrics.*
 tmp=$(mktemp -d /tmp/lyrics.XXX)
 touch $tmp/lyrics.html
 
-if [[ -z $1 ]]; then
-    get_spotify
-else
-    echo "Artist"
-    read -r artist_input
-    
-    echo "Song name"
-    read -r song_input
-    get_manual "$artist_input" "$song_input"
-fi
-
-artist_escaped=$(escape "$artist_raw")
-song_escaped=$(escape "$song_raw")
+artist_escaped=$(escape "$author_name")
+song_escaped=$(escape "$track_name")
 
 both_escaped=$(join "$artist_escaped" "$song_escaped")
 
-override=$(detect_override "$artist" "$song")
+artist=$(adjust "$author_name")
+artist=$(echo $artist | sed -r -e 's/the//gi')
+song=$(adjust "$track_name")
 
-if [[ -z $override ]]; then
-    website="http://www.azlyrics.com/lyrics/"$artist"/"$song".html"
-else 
-    website="http://www.azlyrics.com/lyrics/"$artist"/"$override".html"
-fi
+website="http://www.azlyrics.com/lyrics/"$artist"/"$song".html"
 
 wget -q --header="Accept: text/html" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" -O $tmp/lyrics.html $website
 
